df1323f51a159092d1dfff4fb702f47ddf6e5c2f
Diana <djdiasya2014@gmail.com>
2023-11-01 22:59:05 +0200

    Flask_1

---
+++ main.py
@@ -0,0 +1,46 @@
+from flask import Flask, request
+from utils import generate_requirements, gen_users, space_utils
+
+app = Flask(__name__)
+
+
+@app.route("/requirements")
+def requirements():
+    generate_requirements()
+    with open("requirements.txt", "r") as file:
+        requirements_content = file.readlines()
+    return "<br>".join(requirements_content)
+
+
+@app.route("/generate-users")
+def generate_users():
+    quantity = request.args.get('quantity', '100')
+
+    if quantity.isdigit():
+        quantity = int(quantity)
+        max_quantity = 1000
+
+        if quantity > max_quantity:
+            return f'Quantity should be less then {max_quantity}'
+
+        return gen_users(quantity)
+
+    return f'Invalid quantity value {quantity}'
+
+
+@app.route('/space', methods=['GET'])
+def space():
+    request = space_utils()
+    if request.status_code != 200:
+        return 'Помилка отримання даних зі сторінки', request.status_code
+
+    data = request.json()
+    number = data.get('number', None)
+    if number is not None:
+        return f'Number of astronauts: {number}'
+
+    return 'Поле "number" не знайдено у відповіді з сервера'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
---
+++ utils.py
@@ -0,0 +1,32 @@
+from faker import Faker
+import pkg_resources
+import requests
+
+faker = Faker()
+
+
+def generate_requirements():
+    installed_packages = [(pkg.key, pkg.version) for pkg in pkg_resources.working_set]
+
+    with open("requirements.txt", "w") as file:
+        for package, version in installed_packages:
+            file.write(f"{package}=={version}\n")
+
+    requirements_content = "\n".join([f"{package}=={version}" for package, version in installed_packages])
+    return requirements_content
+
+
+def gen_users(quantity: int = 100) -> str:
+    result = ''
+    for _ in range(quantity):
+        users = faker.name()
+        email = faker.email(users)
+        result += f'<p>Ім\'я: {users}, Електронна адреса: {email}</p>'
+    return result
+
+
+def space_utils():
+    url = 'http://api.open-notify.org/astros.json'
+    response = requests.get(url)
+    return response
+
